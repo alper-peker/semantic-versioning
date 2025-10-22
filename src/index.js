41ee509803d3a7ba2d297c7b36be921f619e4260
alper-peker <alper.peker@taico.io>
2025-10-22 15:11:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ console.log('Semantic Versioning Demo');
 console.log('Testing: Major bump');
 console.log('Testing: Minor version bump');
 console.log('Testing: Patching new minor version');
+console.log('Testing: Merge button');
