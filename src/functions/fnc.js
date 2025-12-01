3297099a6a2cc3586a61a484218ac6db5af38921
Alper Peker <pekeralper98@gmail.com>
2025-12-01 08:10:00 +0100

    Release notes are now generated using the diff from the last release and according to a template.

--- src/functions/fnc.js
+++ src/functions/fnc.js
@@ -1,2 +1,3 @@
 console.log("Testing the label approval for fnc tag")
 console.log("Testing release mgmt and AI generated release notes")
+console.log("Testing the release notes being created with the diff between releases with a template")
