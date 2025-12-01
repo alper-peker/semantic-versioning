e0e05449fa43c854768fba5c268ec63099f71755
Alper Peker <pekeralper98@gmail.com>
2025-12-01 08:54:08 +0100

    testing release note updates and formatting

--- src/functions/fnc.js
+++ src/functions/fnc.js
@@ -1,3 +1,4 @@
 console.log("Testing the label approval for fnc tag")
 console.log("Testing release mgmt and AI generated release notes")
 console.log("Testing the release notes being created with the diff between releases with a template")
+console.log("Testing the release notes being created with the diff between releases with a template 1")
